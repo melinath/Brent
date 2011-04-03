0b2b98beacba98366a4d336a2a806551eb862b65
Stephen Burrows <stephen.r.burrows@gmail.com>
2011-04-03 19:13:28 -0400

    Abstracted the quest_center code into an [interaction] tag.

--- lua/includes.lua
+++ lua/includes.lua
@@ -2,7 +2,10 @@ quest_utils = wesnoth.require "~add-ons/Brent/lua/quests/utils.lua"
 
 --! General
 wesnoth.dofile "~add-ons/Brent/lua/wml_tags.lua"
-wesnoth.dofile "~add-ons/Brent/lua/loading.lua"
+
+
+--! Maps
+wesnoth.dofile "~add-ons/Brent/lua/maps/loading.lua"
 
 --! Quests
 wesnoth.dofile "~add-ons/Brent/lua/quests/loading.lua"
@@ -10,10 +13,6 @@ wesnoth.dofile "~add-ons/Brent/lua/quests/loading.lua"
 
 --! Time
 wesnoth.dofile "~add-ons/Brent/lua/time/loading.lua"
-
-
---! Maps
-wesnoth.dofile "~add-ons/Brent/lua/maps/loading.lua"
 
 
 -- load some files for debugging purposes
--- lua/maps/loading.lua
+++ lua/maps/loading.lua
@@ -1,6 +1,121 @@
 local game_events = wesnoth.game_events
 local map_utils = wesnoth.require "~add-ons/Brent/lua/maps/utils.lua"
 local helper = wesnoth.require "lua/helper.lua"
+local items = wesnoth.require "lua/wml/items.lua"
+
+
+map = {
+	id = nil,
+	turns_per_day = 6,
+	
+	config = function(self, cfg)
+		self.turns_per_day = cfg.turns_per_day or self.turns_per_day
+		self.id = cfg.id
+	end,
+	dump = function(self)
+		return {
+			turns_per_day = self.turns_per_day,
+			id = self.id
+		}
+	end,
+	get_timedelta = function(self)
+		-- timedelta is the local time change per turn
+		return 24/self.turns_per_day
+	end
+}
+
+interactions = {}
+
+--! Interaction base class
+local interaction = {
+	new = function(self, cfg)
+			o = cfg or {}
+			setmetatable(o, self)
+			self.__index = self
+			table.insert(interactions, o)
+			return o
+		end,
+	
+	setup = function(self, cfg)
+		self.cfg = cfg
+		local filter = helper.get_child(cfg, "filter")
+		if (filter == nil) then error("~wml:[interaction] expects a [filter] child", 0) end
+		local command = helper.get_child(cfg, "command")
+		if (command == nil) then error("~wml:[interaction] expects a [command] child", 0) end
+		--if (cfg.id == nil) then error("~wml:[interaction] must declare an id", 0) end
+		self.filter = helper.literal(filter)
+		self.command = helper.literal(command)
+		self.x = cfg.x
+		self.y = cfg.y
+		self.image = cfg.image
+		if self.image and self.x and self.y then
+			items.place_image(self.x, self.y, self.image)
+		end
+		
+		local old_on_event = game_events.on_event
+		function game_events.on_event(name)
+			if old_on_event then old_on_event(name) end
+			self:on_event(name)
+		end
+	end,
+	
+	on_event = function(self, name)
+		if (name == "moveto") then
+			local c = wesnoth.current.event_context
+			local u = wesnoth.get_unit(c.x1, c.y1)
+			if (wesnoth.match_unit(u, self.filter)) then
+				self:activate()
+			end
+		end
+	end,
+	activate = function(self)
+		for i=1,#self.command do
+			local v = self.command[i]
+			wesnoth.fire(v[1], v[2])
+		end
+	end,
+	
+	dump = function(self)
+		return self.cfg
+	end,
+	
+	cfg = nil,
+	filter = nil,
+	x = nil,
+	y = nil,
+	image = nil,
+	command = nil,
+}
+
+
+local old_on_load = game_events.on_load
+function game_events.on_load(cfg)
+	for i=#cfg,1,-1 do
+		local v = cfg[i]
+		if v[1] == "map" then
+			local v2 = v[2]
+			map:config(v2)
+			table.remove(cfg, i)
+		elseif v[1] == "interaction" then
+			local v2 = v[2]
+			local interaction = interaction:new()
+			interaction:setup(v2)
+			table.remove(cfg, i)
+		end
+	end
+	old_on_load(cfg)
+end
+
+
+local old_on_save = game_events.on_save
+function game_events.on_save()
+	cfg = old_on_save()
+	table.insert(cfg, {"map", map:dump()})
+	for i=1,#interactions do
+		table.insert(cfg, {"interaction", interactions[i]:dump()})
+	end
+	return cfg
+end
 
 
 local old_on_event = game_events.on_event
