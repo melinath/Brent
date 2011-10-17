dfa6cc6483dbde524a2d5e95cab19fcf5f522ad4
Stephen Burrows <stephen.r.burrows@gmail.com>
2011-10-16 18:31:01 -0700

    Added some missing includes.

--- lua/includes.lua
+++ lua/includes.lua
@@ -3,6 +3,9 @@ debug_utils = wesnoth.require "~add-ons/Wesnoth_Lua_Pack/debug_utils.lua"
 modular = wesnoth.require "~add-ons/ModularLua/modular.lua"
 
 maps = modular.require "maps"
+inspect = modular.require "inspect"
+markup = modular.require "markup"
+modular.require_tags("exits", "interaction")
 
 --! General
 quests = modular.require("quests", "Brent")
